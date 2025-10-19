5ce6c6a134c3027c12eef0f64f00d51978c264ca
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-10-19 17:32:14 +0600

    navabr style

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     return (
-        <div>
+        <div className=' bg-sky-400 p-4 flex justify-around text-2xl m-5'>
         
                 <Link to={'/home'}>Home</Link>
                 <Link to={'/about'}>About</Link>
